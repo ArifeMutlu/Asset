d0cc012cc9f8515b27d987a502fa0a6c9e11239d
arifemutlu <arife.mutlu@outlook.com>
2018-11-20 20:47:29 +0300

    first commit

--- WebExperience.Test/Controllers/AssetController.cs
+++ WebExperience.Test/Controllers/AssetController.cs
@@ -8,7 +8,7 @@ namespace WebExperience.Test.Controllers
         // Create an API controller via REST to perform all CRUD operations on the asset objects created as part of the CSV processing test
         // Visualize the assets in a paged overview showing the title and created on field
         // Clicking an asset should navigate the user to a detail page showing all properties
-        // Any data repository is permitted
+        // Any data repository is permitted  
         // Use a client MVVM framework
     }
 }
