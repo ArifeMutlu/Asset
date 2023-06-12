febb0eaf909329b5191a9d7fb89e19b843dad3b6
ArifeMutlu <arife.mutlu@outlook.com>
2023-06-12 14:48:42 +0300

    Update AssetController.cs

--- WebExperience.Test/Controllers/AssetController.cs
+++ WebExperience.Test/Controllers/AssetController.cs
@@ -17,14 +17,7 @@ using WebExperience.Test.Models.dtos;
 namespace WebExperience.Test.Controllers
 {
     public class AssetController : ApiController
-    {
-        // TODO
-        // Create an API controller via REST to perform all CRUD operations on the asset objects created as part of the CSV processing test
-        // Visualize the assets in a paged overview showing the title and created on field
-        // Clicking an asset should navigate the user to a detail page showing all properties
-        // Any data repository is permitted  
-        // Use a client MVVM framework
-
+    {    
         ApplicationDbContext _db;
         public AssetController()
         {
